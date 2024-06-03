6294daab1bd9a7533c151691ccc0dc8a1610ec8f
Liam <jeancarlos.ramos@live.com>
2024-06-03 11:02:05 -0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,10 +1,7 @@
-pip install selenium
-pip install webdriver-manager
-pip install chromedriver-binary
-cls
 @echo off
 title BOT SERVER CONSOLE
 :start
 echo Bem vindo ao comprasnet bot server console
-call ambiente_virtual\Scripts\activate && echo Aperte ENTER para iniciar
-pause >nul && call python bot_multi.py
+call C:\ambiente_virtual\Scripts\activate
+echo Aperte ENTER para iniciar
+pause >nul && call python bot_one.py
