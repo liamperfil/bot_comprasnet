2e2f4604003099aac688302df7ec1487945f2ed0
Liam <jeancarlos.ramos@live.com>
2024-06-03 12:22:29 -0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,6 +2,6 @@
 title BOT SERVER CONSOLE
 :start
 echo Bem vindo ao comprasnet bot server console
+echo Seu programa vai iniciar
 call C:\ambiente_virtual\Scripts\activate
-echo Aperte ENTER para iniciar
-pause >nul && call python bot_one.py
+call python bot_one.py
