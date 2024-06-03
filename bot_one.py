a50eaf4e32d4e506f52e9eaadb4ce548307c664a
Liam <jeancarlos.ramos@live.com>
2024-06-03 12:22:25 -0300

    Update bot_one.py

--- bot_one.py
+++ bot_one.py
@@ -19,41 +19,45 @@ num_dispensa = input("Digite o número da dispensa: ")
 item = 1
 meu_preco = input(f"Digite o preço do item {item}: ")
 meu_preco = conv_float(meu_preco)
+print(f'Confira seu preço, você digitou: {meu_preco}')
 
 # Inicializa o navegador webdriver Chrome E Abre uma página da web
 navegador = webdriver.Chrome(service=servico)
 navegador.get("https://comprasnet3.ba.gov.br/Fornecedor/LoginDispensa.asp?txtFuncionalidade=&txtNumeroDispensa=" + num_dispensa)
 
 xpath_status_disputa = '//*[@id="frmCotarCotacaoEmDisputa"]/table/tbody/tr[' + str(4 * item) + ']/td[8]/span'
-xpath_checar_preco = '//*[@id="frmCotarCotacaoEmDisputa"]/table/tbody/tr[' + str(4 * item) + ']/td[7]'
+xpath_preco_registrado = '//*[@id="frmCotarCotacaoEmDisputa"]/table/tbody/tr[' + str(4 * item) + ']/td[7]'
 xpath_frm_preco = '//*[@id="txtFrmPreco' + str(item-1) + '"]'
 
-print('\033[1;34;40m Credenciamento manual \033[m')
-input('Pressione qualquer tecla para continuar...')
+print('\033[1;34;40m Preencha a autenticação no site \033[m')
+print('\033[1;34;40m Após credenciar pressione qualquer tecla para continuar... \033[m')
+input('')
 
 while True:
     try:
         status_disputa = navegador.find_element("xpath", xpath_status_disputa).text # Texto Você vence! ou Você perde!
-        checar_preco = navegador.find_element("xpath", xpath_checar_preco).text # recebe preço em string
-        checar_preco = conv_float(checar_preco) # converter preço em número
+        preco_registrado = navegador.find_element("xpath", xpath_preco_registrado).text # recebe preço em string
+        preco_registrado = conv_float(preco_registrado) # converter preço em número
     except:
         break
     
-    while (status_disputa == "Você perde!" and checar_preco > meu_preco):
+    while (status_disputa == "Você perde!" and preco_registrado > meu_preco):
         try:
-            checar_preco -= uniform(0.01, 1) # Lance entre 1 centavo e 1 real para não ficar uniforme
+            preco_registrado -= uniform(0.01, 0.10) # Lance entre 1 centavo e 1 real para não ficar uniforme
             frm_preco = navegador.find_element("xpath", xpath_frm_preco) # Campo formulario de lance
-            frm_preco.send_keys("{:.4f}".format(checar_preco)) # Enviando lances com 4 casas decimais
+            frm_preco.clear()
+            frm_preco.send_keys("{:.4f}".format(preco_registrado)) # Enviando lances com 4 casas decimais
             btn_enviar_preco = navegador.find_element("xpath", '//*[@id="btnCotarPrecoRodape"]').click() # Botao cotar preço
             WebDriverWait(navegador, 300).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="btnCotarPrecoRodape"]')))
             status_disputa = navegador.find_element("xpath", xpath_status_disputa).text # Atualiza Você vence! ou Você perde!
-            checar_preco = navegador.find_element("xpath", xpath_checar_preco).text # Atualiza o preço
-            checar_preco = conv_float(checar_preco) # Converte preço em número
+            preco_registrado = navegador.find_element("xpath", xpath_preco_registrado).text # Atualiza o preço
+            preco_registrado = conv_float(preco_registrado) # Converte preço em número
         except:
             break
     
-    if (checar_preco < meu_preco):
-        print('PREÇO ABAIXO DO VALOR REGISTRADO!!!')
+    if (preco_registrado < meu_preco):
+        print('\033[1;33;41m PREÇO ABAIXO DO VALOR REGISTRADO!!! \033[m')
+        break
 
     while (status_disputa == "Você vence!"):
         navegador.refresh()
@@ -70,4 +74,4 @@ while True:
     except:
         break
 
-input('Pressione qualquer tecla para encerrar...')
+input('\033[7;35;40m Pressione qualquer tecla para encerrar... \033[m')
