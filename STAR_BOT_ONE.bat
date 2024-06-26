69b9125332bb953292a366ce8fe98aa183a35ccb
Liam <jeancarlos.ramos@live.com>
2024-06-26 09:43:21 -0300

    Create STAR_BOT_ONE.bat

---
+++ STAR_BOT_ONE.bat
@@ -0,0 +1,7 @@
+@echo off
+title BOT SERVER CONSOLE
+:start
+echo Bem vindo ao comprasnet bot server console
+echo Seu programa vai iniciar
+call C:\ambiente_virtual\Scripts\activate
+call python bot_one.py
