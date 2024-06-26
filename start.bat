3d7a6784dddb972dfe4a2a068ed6c3267d5fe48f
Liam <jeancarlos.ramos@live.com>
2024-06-26 09:43:27 -0300

    Delete start.bat

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title BOT SERVER CONSOLE
-:start
-echo Bem vindo ao comprasnet bot server console
-echo Seu programa vai iniciar
-call C:\ambiente_virtual\Scripts\activate
-call python bot_one.py
